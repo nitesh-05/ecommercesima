9aaa0a3c241a808a5f621e644471f804ca3eac78
nitesh-05 <niteshgts05@gmail.com>
2023-10-28 20:52:00 +0530

    backend_ change minor

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -3,12 +3,15 @@ import Layout from "./../components/Layouts/Layout";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 import "../styles/ProductDetailsStyles.css";
+import { toast } from "react-toastify";
+import { useCart } from "../context/Cart";
 
 const ProductDetails = () => {
   const params = useParams();
   const navigate = useNavigate();
   const [product, setProduct] = useState({});
   const [relatedProducts, setRelatedProducts] = useState([]);
+  const [cart, setCart] = useCart();
 
   //initalp details
   useEffect(() => {
@@ -62,7 +65,17 @@ const ProductDetails = () => {
             })}
           </h6>
           <h6>Category : {product?.category?.name}</h6>
-          <button class="btn btn-secondary ms-1">ADD TO CART</button>
+          <button class="btn btn-secondary ms-1" 
+          
+          onClick={() => {
+            setCart([...cart, product]);
+            localStorage.setItem(
+              "cart",
+              JSON.stringify([...cart, product])
+            );
+            toast.success("Item Added to cart");
+          }}
+          >ADD TO CART</button>
         </div>
       </div>
       <hr />
@@ -99,7 +112,7 @@ const ProductDetails = () => {
                   >
                     More Details
                   </button>
-                  {/* <button
+                  <button
                   className="btn btn-dark ms-1"
                   onClick={() => {
                     setCart([...cart, p]);
@@ -111,7 +124,7 @@ const ProductDetails = () => {
                   }}
                 >
                   ADD TO CART
-                </button> */}
+                </button>
                 </div>
               </div>
             </div>
